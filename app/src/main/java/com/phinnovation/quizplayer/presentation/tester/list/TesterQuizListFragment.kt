48b49b5e6c3c08da231bff056b5ba40f140edbe3
Fotis Chatzinikos <fotis.chatzinikos@gmail.com>
2020-10-12 14:19:00 +0300

    Refactored tester.list screen to use the new Event class that keeps track if the event has been consumed or not. Cleaned up the fragment, moved all functionality in the view model

--- app/src/main/java/com/phinnovation/quizplayer/presentation/tester/list/TesterQuizListFragment.kt
+++ app/src/main/java/com/phinnovation/quizplayer/presentation/tester/list/TesterQuizListFragment.kt
@@ -2,7 +2,6 @@ package com.phinnovation.quizplayer.presentation.tester.list
 
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,13 +10,12 @@ import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import com.phinnovation.core.domain.QuizState
 import com.phinnovation.quizplayer.R
 import com.phinnovation.quizplayer.framework.QuizPlayerViewModelFactory
 import com.phinnovation.quizplayer.presentation.MainActivityDelegate
 import kotlinx.android.synthetic.main.fragment_tester_list.*
 
-class TesterQuizListFragment : Fragment () {
+class TesterQuizListFragment : Fragment() {
 
     private lateinit var mainActivityDelegate: MainActivityDelegate
 
@@ -48,49 +46,45 @@ class TesterQuizListFragment : Fragment () {
             .get(TesterQuizListViewModel::class.java)
 
         val adapter = TesterQuizListAdapter() {
-
             //Adapter click handler
-            viewModel.setOpenQuiz(it)
-
-            if (it.state == QuizState.FINISHED) {
-                Toast.makeText(context,"You cannot retake a finished quiz!",Toast.LENGTH_SHORT).show()
-            } else {
-                viewModel.checkIfQuizHasQuestionsAndThusCanBeOpened(it)
-            }
+            viewModel.checkQuizCanBeOpened(it)
         }
 
-        viewModel.hasQuestionsCanBeOpened.observe(viewLifecycleOwner, Observer {
-            if (it==null) {
-                Log.d("TesterQuizFragment", "!!!!!!!!!!!!!!!!!!!Skipping null")
-                return@Observer
-            }
+        quizRecyclerView.adapter = adapter
 
-            if (it) {
-                viewModel.hasQuestionsCanBeOpened.value = null
+        viewModel.navigateToQuizEvent.observe(viewLifecycleOwner, Observer {
+            it.getContentIfNotHandled()?.let { // Only proceed if the event has never been handled
                 mainActivityDelegate.openQuizForPlayIntro()
-            } else {
+            }
+        })
+
+        viewModel.showEmptyQuestionsErrorEvent.observe(viewLifecycleOwner, Observer {
+            it.getContentIfNotHandled()?.let { // Only proceed if the event has never been handled
                 showErrorMisconfiguredQuiz()
             }
         })
 
-        quizRecyclerView.adapter = adapter
-
+        viewModel.showQuizFinishedErrorEvent.observe(viewLifecycleOwner, Observer {
+            it.getContentIfNotHandled()?.let { // Only proceed if the event has never been handled
+                Toast.makeText(context, "You cannot retake a finished quiz!", Toast.LENGTH_SHORT).show()
+            }
+        })
 
         viewModel.quizzes.observe(viewLifecycleOwner, Observer {
             if (it.isNotEmpty()) {
                 emptyQuizesLabel.visibility = View.GONE
             }
-            adapter.update(it) }
-        )
-        viewModel.loadQuizzes()
+            adapter.update(it)
+        })
 
+        viewModel.loadQuizzes()
     }
 
     private fun showErrorMisconfiguredQuiz() {
         return AlertDialog.Builder(requireContext())
             .setTitle("Error!")
             .setMessage("At least one question must be set before testing the Quiz!")
-            .setPositiveButton(android.R.string.ok,null)
+            .setPositiveButton(android.R.string.ok, null)
             .create().show()
     }
 
--- app/src/main/java/com/phinnovation/quizplayer/presentation/tester/list/TesterQuizListViewModel.kt
+++ app/src/main/java/com/phinnovation/quizplayer/presentation/tester/list/TesterQuizListViewModel.kt
@@ -1,19 +1,33 @@
 package com.phinnovation.quizplayer.presentation.tester.list
 
 import android.app.Application
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.phinnovation.core.domain.Quiz
+import com.phinnovation.core.domain.QuizState
 import com.phinnovation.quizplayer.framework.Interactors
 import com.phinnovation.quizplayer.framework.QuizPlayerViewModel
+import com.phinnovation.quizplayer.presentation.utils.Event
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
-class TesterQuizListViewModel (application: Application, interactors: Interactors):
-        QuizPlayerViewModel(application,interactors) {
+class TesterQuizListViewModel(application: Application, interactors: Interactors) :
+    QuizPlayerViewModel(application, interactors) {
+
+    private val _navigateToQuiz = MutableLiveData<Event<Boolean>>()
+    private val _showEmptyQuestionsError = MutableLiveData<Event<Boolean>>()
+    private val _showQuizFinishedError = MutableLiveData<Event<Boolean>>()
+
+    val navigateToQuizEvent: LiveData<Event<Boolean>>
+        get() = _navigateToQuiz
+
+    val showEmptyQuestionsErrorEvent: LiveData<Event<Boolean>>
+        get() = _showEmptyQuestionsError
+
+    val showQuizFinishedErrorEvent: LiveData<Event<Boolean>>
+        get() = _showQuizFinishedError
 
     val quizzes: MutableLiveData<List<Quiz>> = MutableLiveData()
-
-    val hasQuestionsCanBeOpened: MutableLiveData<Boolean> = MutableLiveData()
 
     fun loadQuizzes() {
         GlobalScope.launch {
@@ -21,15 +35,28 @@ class TesterQuizListViewModel (application: Application, interactors: Interactor
         }
     }
 
-    fun checkIfQuizHasQuestionsAndThusCanBeOpened(quiz:Quiz) {
-        GlobalScope.launch {
-            val questions = interactors.getQuestions(quiz)
+    fun checkQuizCanBeOpened(quiz: Quiz) {
 
-            hasQuestionsCanBeOpened.postValue(questions.isNotEmpty())
+        if (quiz.state == QuizState.FINISHED) {
+            _showQuizFinishedError.value = Event(true)
+        } else {
+            GlobalScope.launch {
+                val questions = interactors.getQuestions(quiz)
+
+                if (questions.isNotEmpty()) {
+                    //we will continue to the next screen
+                    //set openQuiz and post navigateToQuiz
+                    setOpenQuiz(quiz)
+                    _navigateToQuiz.postValue(Event(true))
+                } else {
+                    //no questions, quiz is not setup yet, post error
+                    _showEmptyQuestionsError.postValue(Event(true))
+                }
+            }
         }
     }
 
-    fun setOpenQuiz(quiz: Quiz) {
+    private fun setOpenQuiz(quiz: Quiz) {
         interactors.setOpenQuiz(quiz)
     }
 }
