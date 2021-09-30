78301238656d81e5766f2211abd48038549fc8fb
Fotis Chatzinikos <fotis.chatzinikos@gmail.com>
2021-10-01 00:11:32 +0300

    re structure app packages

--- app/src/main/java/com/phinnovation/quizplayer/framework/application/QuizPlayerViewModel.kt
+++ app/src/main/java/com/phinnovation/quizplayer/framework/application/QuizPlayerViewModel.kt
@@ -2,6 +2,7 @@ package com.phinnovation.quizplayer.framework
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import com.phinnovation.quizplayer.framework.application.QuizPlayerApplication
 
 open class QuizPlayerViewModel(application: Application, protected val interactors: Interactors) :
     AndroidViewModel(application) {
